c9a7a63ce42acdf29fc6721a7292d1039eae2e24
Andreas Schnapp <a.snap@t-online.de>
2024-03-18 11:31:20 +0100

    make it independent of tx

--- src/main/java/com/example/r2dbcissue531reproducer/service/TestEntityService.java
+++ src/main/java/com/example/r2dbcissue531reproducer/service/TestEntityService.java
@@ -3,22 +3,18 @@ package com.example.r2dbcissue531reproducer.service;
 import com.example.r2dbcissue531reproducer.entity.TestEntity;
 import com.example.r2dbcissue531reproducer.repository.TestEntityRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.reactive.TransactionalOperator;
 import reactor.core.publisher.Mono;
 
 @Service
 public class TestEntityService {
-    private final TransactionalOperator transactionalOperator;
     private final TestEntityRepository testEntityRepository;
 
-    public TestEntityService(TransactionalOperator transactionalOperator, TestEntityRepository testEntityRepository) {
-        this.transactionalOperator = transactionalOperator;
+    public TestEntityService(TestEntityRepository testEntityRepository) {
         this.testEntityRepository = testEntityRepository;
     }
 
     public Mono<TestEntity> doProblematicSequence(int round) {
-        return transactionalOperator
-                .transactional(doProblematicSequenceWithouttx(round));
+        return doProblematicSequenceWithouttx(round);
     }
 
     private Mono<TestEntity> doProblematicSequenceWithouttx(int round) {
